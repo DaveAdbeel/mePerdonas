672aa693781597c2e432fcde5c27d233428aa4ba
David Astudillo <davidadbeelgonzalez@gmail.com>
2021-12-05 22:00:58 -0300

    Creando sitio web

---
+++ README.md
@@ -0,0 +1,2 @@
+# Me-perdonas-
+# Me-perdonas-
---
+++ app.js
@@ -0,0 +1,22 @@
+const btnSi = document.getElementById("btnSi");
+const btnNo = document.getElementById("btnNo");
+const HIDDEN1 = document.getElementById("hidden1");
+const HIDDEN2 = document.getElementById("hidden2");
+const SHOW = document.getElementById("show");
+
+btnSi.addEventListener("click", () => {
+    HIDDEN1.classList.add("hidden");
+    HIDDEN2.classList.add("hidden");
+    SHOW.style.zIndex = "2";
+});
+
+btnNo.addEventListener("mouseover", () => {
+  const posX = Math.floor(Math.random() * 75);
+  const posY = Math.floor(Math.random() * 75);
+
+  btnNo.style.left = `${posX}%`;
+  btnNo.style.top = `${posY}%`;
+  btnNo.style.transition = "all 0.1s";
+});
+
+
